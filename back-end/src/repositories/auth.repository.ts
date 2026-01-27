941aaf0c061f99b35383e7c3d5fdae2d6e6bdd58
David Oliveira <davidolisoa@gmail.com>
2026-01-27 18:13:35 -0300

    feat: adicionando login e logout com token jwt

--- back-end/src/repositories/auth.repository.ts
+++ back-end/src/repositories/auth.repository.ts
@@ -1,29 +1,55 @@
-import {db} from "../database/db";
+import { db } from "../database/db";
 import { User, UserCreateDto } from "../interfaces/user";
 
 export class AuthRepository {
-    static create(userData: UserCreateDto, hashedPassword: string) {
-        const result = db.prepare(`INSERT INTO users (name, email, password)
-      VALUES (?, ?, ?)`).run(userData.name, userData.email, hashedPassword);
-        if (result.changes === 0) {
-            throw new Error('Falha ao inserir usuário no banco de dados');
-        } 
-        return result.lastInsertRowid as number;
+  static create(userData: UserCreateDto, hashedPassword: string) {
+    const result = db
+      .prepare(
+        `INSERT INTO users (name, email, password)
+      VALUES (?, ?, ?)`,
+      )
+      .run(userData.name, userData.email, hashedPassword);
+    if (result.changes === 0) {
+      throw new Error("Falha ao inserir usuário no banco de dados");
     }
-    static exists(email: string): boolean {
-    const result = db.prepare(`
+    return result.lastInsertRowid as number;
+  }
+
+  static exists(email: string): boolean {
+    const result = db
+      .prepare(
+        `
       SELECT COUNT(*) as count
       FROM users
       WHERE email = ?
-    `).get(email) as { count: number };
+    `,
+      )
+      .get(email) as { count: number };
 
     return result.count > 0;
   }
+
   static findById(id: number): User | undefined {
-    return db.prepare(`
+    return db
+      .prepare(
+        `
       SELECT id, name, email, password
       FROM users
       WHERE id = ?
-    `).get(id) as User | undefined;
+    `,
+      )
+      .get(id) as User | undefined;
+  }
+
+  static findByEmail(email: string): User | undefined {
+    return db
+      .prepare(
+        `
+    SELECT id, name, email, password
+    FROM users
+    WHERE email = ?
+  `,
+      )
+      .get(email) as User | undefined;
   }
 }

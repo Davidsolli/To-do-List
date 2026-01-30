e8f12e893249a92ff66d16fb43bf49f53b217c38
Levi <pedrolevi427@gmail.com>
2026-01-30 01:09:56 -0300

    feat: configurar rotas para as novas visualizações de projeto e Kanban

--- front-end/src/routes.ts
+++ front-end/src/routes.ts
@@ -3,6 +3,8 @@ import { RouteDefinition } from './core/Router';
 import { Placeholder } from './views/Placeholder';
 import { LoginView } from './views/Auth/LoginView';
 import { RegisterView } from './views/Auth/RegisterView';
+import { ProjectView } from './views/Project/ProjectView'; // Lista de projetos
+import { KanbanView } from './views/Project/KanbanView';
 
 //roles possíveis: admin e user (padrão)
 //protected - precisa de autenticação
@@ -11,4 +13,6 @@ export const routes: RouteDefinition[] = [
     { path: '/login', view: LoginView },
     { path: '/register', view: RegisterView },
     { path: '/admin', view: Placeholder, protected: true, roles: ['admin'] },
+    { path: '/projects', view: ProjectView, protected: true },
+    { path: '/project', view: KanbanView, protected: true },
 ];
